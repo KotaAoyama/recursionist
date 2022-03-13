e6f3e1c92c50651bd7556a68197738b196ff7084
Kota Aoyama <hongcha811@gmail.com>
2022-03-14 07:56:39 +0900

    first lesson

---
+++ csBasic/src/booleanAndInt.java
@@ -0,0 +1,9 @@
+public class booleanAndInt {
+    public static void main(String[] args) {
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(9999);
+        System.out.println(Integer.parseInt("1111",2));
+        System.out.println(Integer.parseInt("11", 2));
+    }
+}
