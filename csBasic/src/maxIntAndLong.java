8bf08cca42be90fd4c204c01146394ade37269fd
Kota Aoyama <hongcha811@gmail.com>
2022-03-15 07:30:44 +0900

    added long

--- csBasic/src/maxIntAndLong.java
+++ csBasic/src/maxIntAndLong.java
@@ -1,4 +1,4 @@
-public class maxInt {
+public class maxIntAndLong {
     public static void main(String[] args) {
         // プログラムの中で整数を扱う際は、Java の中では特に指定がない場合、数値は int 型として扱われました
         // Java では int 型は、32ビット（4 バイト）です
@@ -17,5 +17,11 @@ public class maxInt {
 
         // integer number too large とエラーメッセージが表示されます
         // これは文字通り整数の値が大きすぎることを意味します
+
+        // int 型では 2147483647 を超えるとエラーが発生しました
+        // Java では long 型は 64 ビット（8 バイト）です
+        // long 型を示す L を末尾につけることで、コンピュータは long 型と認識します
+        // L を付けないと、数値は int 型と判断され、エラーになります
+        System.out.println(2147483648L);
     }
 }
