dd2a2686090b8b48868f2dcf708d66cd44ae8a5d
Kota Aoyama <hongcha811@gmail.com>
2022-03-15 07:46:23 +0900

    hexadecimal syntax

---
+++ csBasic/src/hexadecimal.java
@@ -0,0 +1,8 @@
+public class hexadecimal {
+    public static void main(String[] args){
+        System.out.println(0xb);
+        System.out.println(0xff);
+        System.out.println(0x11);
+        System.out.println(0x90);
+    }
+}
