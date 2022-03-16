f3d3aba8f6047b1d31d703b2121d6600984e4f4d
Kota Aoyama <hongcha811@gmail.com>
2022-03-17 07:27:09 +0900

    string

---
+++ csBasic/src/String.java
@@ -0,0 +1,16 @@
+public class String {
+    public static void main(String[] args) {
+        // コンピュータに文字と認識してもらうために、文字列にはダブルクオテーション(")を使います
+        // 文字列型
+        System.out.println("abcd");
+        System.out.println("abcdef");
+
+        // 文字であっても、ダブルクオテーションを使うことによって、文字列として認識されます
+        System.out.println("a");
+
+        System.out.println("The earth was bluish.");
+        System.out.println("");
+        System.out.println(1 + 1);
+        System.out.println("1" + "1");
+    }
+}
