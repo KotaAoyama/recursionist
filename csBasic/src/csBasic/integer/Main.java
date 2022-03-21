58ec36f32861cc0e632900a889563051f771b631
Kota Aoyama <hongcha811@gmail.com>
2022-03-21 09:32:31 +0900

    integer

---
+++ csBasic/src/csBasic/integer/Main.java
@@ -0,0 +1,19 @@
+package csBasic.integer;
+
+public class Main {
+    public static void main(String[] args) {
+        // Java の整数には、byte　型, short　型, int 型, long 型が存在します
+
+        // int 型（32 ビット）
+        System.out.println(Integer.MAX_VALUE);
+
+        // byte 型（8 ビット）
+        System.out.println(Byte.MAX_VALUE);
+
+        // short 型（16ビット）
+        System.out.println(Short.MAX_VALUE);
+
+        // long 型（64ビット）
+        System.out.println(Long.MAX_VALUE);
+    }
+}
