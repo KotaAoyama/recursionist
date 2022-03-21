8ef2a33a3831183f268b5904078d8840c3f0f98a
Kota Aoyama <hongcha811@gmail.com>
2022-03-21 20:54:06 +0900

    string index

--- csBasic/src/csBacis/string/Main.java
+++ csBasic/src/csBacis/string/Main.java
@@ -14,5 +14,13 @@ public class Main {
         System.out.println("");
         System.out.println(1 + 1);
         System.out.println("1" + "1");
+
+        // Java では、charAt メソッドが使われます
+        // String.charAt(int)
+        // 文字型が返されます
+        // Python JavaScriptなどだと、文字列[インデックス]
+        System.out.println("Recursion".charAt(0));
+        System.out.println("Recursion".charAt(4));
+        System.out.println("Recursion".charAt(8));
     }
 }
