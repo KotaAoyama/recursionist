a029ac747f92eda9e9240cf221a7d3f918dc1a7d
Kota Aoyama <hongcha811@gmail.com>
2022-04-02 07:30:01 +0900

    added gcd

---
+++ csBasic/src/csMiddle/recursion/gcd/Main.java
@@ -0,0 +1,13 @@
+package csMiddle.recursion.gcd;
+
+public class Main {
+    public static int maximumPeople(int x, int y){
+        if(x % y == 0) return y;
+        return maximumPeople(y, x%y);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maximumPeople(1029,1071)); // 21
+        System.out.println(maximumPeople(3180,1908)); // 636
+    }
+}
