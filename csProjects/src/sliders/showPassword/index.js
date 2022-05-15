bed619cd550cb2500ae1662e8537fa5c9aa2ecd6
Kota Aoyama <hongcha811@gmail.com>
2022-05-16 07:52:01 +0900

    lock Password

--- csProjects/src/sliders/showPassword/index.js
+++ csProjects/src/sliders/showPassword/index.js
@@ -2,3 +2,13 @@ function passwordToggle(){
     const pass = document.getElementById("passwordClick");
     pass.type = pass.type === "password" ? "text" : "password";
 }
+
+function lockPassword() {
+    const passwordInput = document.getElementById('passwordClick');
+    passwordInput.disabled = true;
+
+    const p = document.createElement('p');
+    const lockedPasswordDiv = document.getElementById('lockedPassDiv') ;
+    p.append(passwordInput.value);
+    lockedPasswordDiv.append(p);
+}
