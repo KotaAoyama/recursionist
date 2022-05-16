6f90b057137124dcb370c5ace61d3d45d7e25e74
Kota Aoyama <hongcha811@gmail.com>
2022-05-17 07:52:57 +0900

    mouseover & mouseout

--- csProjects/src/sliders/showPassword/index.js
+++ csProjects/src/sliders/showPassword/index.js
@@ -7,8 +7,21 @@ function lockPassword() {
     const passwordInput = document.getElementById('passwordClick');
     passwordInput.disabled = true;
 
+    if(passwordInput.value === "") return;
+
     const p = document.createElement('p');
     const lockedPasswordDiv = document.getElementById('lockedPassDiv') ;
     p.append(passwordInput.value);
+
+    let saved = passwordInput.value;
+    p.addEventListener('mouseover', function () {
+        p.innerText = saved;
+    })
+    p.addEventListener('mouseout', function() {
+        p.innerText = "*".repeat(p.innerText.length);
+    })
+
+    passwordInput.value = "";
     lockedPasswordDiv.append(p);
 }
+
