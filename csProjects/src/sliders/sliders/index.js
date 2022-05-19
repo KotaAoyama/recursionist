e52a83b8a922462bae4be2d44f86480e4ca2a99b
Kota Aoyama <hongcha811@gmail.com>
2022-05-20 07:44:26 +0900

    updated sliders

--- csProjects/src/sliders/sliders/index.js
+++ csProjects/src/sliders/sliders/index.js
@@ -1,25 +1,13 @@
 const target = document.getElementById("target");
-
 const sliderItems = document.querySelectorAll("#target .slider-data .slider-item");
-let index = 0;
-// console.log(sliderItems);
-// console.log(sliderItems[index]);
 
 const sliderShow = document.createElement("div");
 const main = document.createElement("div");
 const extra = document.createElement("div");
-const arrows = document.createElement("div");
-const left = document.createElement("button");
-const right = document.createElement("button");
 
-sliderShow.classList.add("col-12", "d-flex", "flex-nowrap", "overflow-hidden");
+sliderShow.classList.add("col-12", "d-flex", "flex-nowrap", "overflow-hiddens");
 main.classList.add("main", "full-width");
 extra.classList.add("extra", "full-width");
-arrows.classList.add("offset-5", "mt-2");
-left.classList.add("btn", "btn-light");
-left.innerText = "<";
-right.classList.add("btn", "btn-light");
-right.innerText = ">";
 
 main.append(sliderItems[0]);
 
@@ -27,39 +15,52 @@ sliderShow.append(main);
 sliderShow.append(extra);
 target.append(sliderShow);
 
-arrows.append(left, right);
-target.append(arrows);
+const controls = document.createElement("div");
+controls.classList.add("offset-5", "mt-2");
 
-// 現在のindexにdata-indexを設定します。
-// setAttribute(属性名, 属性値)
+const leftBtn = document.createElement("button");
+leftBtn.classList.add("btn", "btn-light");
+leftBtn.innerHTML = "<";
+
+const rightBtn = document.createElement("button");
+rightBtn.classList.add("btn", "btn-light");
+rightBtn.innerHTML = ">";
+
+controls.append(leftBtn);
+controls.append(rightBtn);
+target.append(controls);
+
 main.setAttribute("data-index", "0");
 
-// 1か-1を受け取って次の要素を設定するslideJumpという関数を作成します。
 function slideJump(steps) {
-    // 現在のindexを取得します。
     let index = parseInt(main.getAttribute("data-index"));
-    // 現在の要素
-    let currentElement = sliderItems.item(index);
+    const currentElement = sliderItems.item(index);
 
-    // 受け取ったstepに応じてindexを変化させます。
     index += steps;
-    console.log(index);
 
-    // 周回させるように
     if(index < 0) index = sliderItems.length -1;
     else if(index >= sliderItems.length) index = 0;
 
-    // 上でindexがstepによって更新されているので、次の要素を設定します。
-    let nextElement = sliderItems.item(index);
+    const nextElement = sliderItems.item(index);
 
-    // コンソールで確認
-    console.log(currentElement);
-    console.log(nextElement);
-
-    // indexが更新されたのでdata-indexの更新
     main.setAttribute("data-index", index.toString());
 }
 
-// blue-gray-success-yellow-redで並んでいます。
-slideJump(1);　//blue, grayが出力されます
-slideJump(-1); //gray, blueが出力されます
+function animateMain(currentElement, nextElement, animationType) {
+    // extra.innerHTML = "";
+    extra.append(currentElement);
+
+    // main.innerHTML = "";
+    main.append(nextElement);
+
+    main.classList.add("expand-animation");
+    extra.classList.add("deplete-animation");
+
+    if (animationType == "right") {
+        // sliderShow.innerHTML = "";
+        sliderShow.append(extra);
+        sliderShow.append(main);
+    }
+}
+
+animateMain(sliderItems[0], sliderItems[1], "right");
