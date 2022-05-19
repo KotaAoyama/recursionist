b0e4fbb4247499b9e54cbf596ddd717a7c3939b8
Kota Aoyama <hongcha811@gmail.com>
2022-05-19 21:31:33 +0900

    added arrows

--- csProjects/src/sliders/sliders/index.js
+++ csProjects/src/sliders/sliders/index.js
@@ -8,13 +8,24 @@ console.log(sliderItems[0]);
 const sliderShow = document.createElement("div");
 const main = document.createElement("div");
 const extra = document.createElement("div");
+const arrows = document.createElement("div");
+const left = document.createElement("button");
+const right = document.createElement("button");
 
-sliderShow.classList.add("col-12", "d-flex", "flex-nowrap", "overflow-hiddens");
+sliderShow.classList.add("col-12", "d-flex", "flex-nowrap", "overflow-hidden");
 main.classList.add("main", "full-width");
 extra.classList.add("extra", "full-width");
+arrows.classList.add("offset-5", "mt-2");
+left.classList.add("btn", "btn-light");
+left.innerText = "<";
+right.classList.add("btn", "btn-light");
+right.innerText = ">";
 
 main.append(sliderItems[0]);
 
 sliderShow.append(main);
 sliderShow.append(extra);
 target.append(sliderShow);
+
+arrows.append(left, right);
+target.append(arrows);
