2bd5975db4688d5d628f87e7da9ff37aac29b590
Kota Aoyama <hongcha811@gmail.com>
2022-05-26 23:48:36 +0900

    insert Node

--- csBasic/src/csAdvanced/singlyLinkedList/Main.java
+++ csBasic/src/csAdvanced/singlyLinkedList/Main.java
@@ -49,8 +49,15 @@ class Main{
         int[] arr = new int[]{35,23,546,67,86,234,56,767,34,1,98,78,555};
         SinglyLinkedList numList = new SinglyLinkedList(arr);
 
+        // インデックス2と3の間に40という値を挿入します。
+        Node thirdEle = numList.at(2);
+        Node tempNode = thirdEle.next;
+        Node newNode = new Node(40);
+        thirdEle.next = newNode;
+        newNode.next = tempNode;
+
         System.out.println(numList.at(2).data);
-        System.out.println(numList.at(5).data);
+        System.out.println(numList.at(3).data);
         System.out.println(numList.findNode(67));
         System.out.println(numList.findNode(767));
 
