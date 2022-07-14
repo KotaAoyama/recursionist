c69e4866669811b6065f690ef0b8a0a0f9dc0f4e
Kota Aoyama <hongcha811@gmail.com>
2022-07-14 23:13:51 +0900

    updated main

--- csBasic/src/csMiddle/list/sumOfArray/Main.java
+++ csBasic/src/csMiddle/list/sumOfArray/Main.java
@@ -1,27 +1,28 @@
 package csMiddle.list.sumOfArray;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
-    public static int sumOfArrayHelper(List<Integer> list, int start, int end) {
-        if (start == end) return list.get(start);
+    public static int sumOfArrayHelper(List<Integer> arr, int start, int end) {
+        if (start == end) return arr.get(start);
 
         int middle = (start + end) / 2;
 
-        int sumOfRightList = sumOfArrayHelper(list, middle+1, end);
-        int sumOfLeftList = sumOfArrayHelper(list, start, middle);
+        int sumOfRightList = sumOfArrayHelper(arr, middle+1, end);
+        int sumOfLeftList = sumOfArrayHelper(arr, start, middle);
 
         return sumOfLeftList + sumOfRightList;
     }
 
-    public static int sumOfArray(int[] arr){
-        List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
-        return sumOfArrayHelper(list, 0, list.size()-1);
+    public static int sumOfArray(List<Integer> arr){
+        return sumOfArrayHelper(arr, 0, arr.size()-1);
     }
 
     public static void main(String[] args) {
-        System.out.println(sumOfArray(new int[]{2,4,6,8,10}));
+        List<Integer> arr = new ArrayList<>(Arrays.asList(2,4,6,8,10,12));
+        System.out.println(sumOfArray(arr));
     }
 }
