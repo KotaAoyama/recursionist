48a80dda62be0d37862c6cd1e2c190ae3bb8bc7b
Kota Aoyama <hongcha811@gmail.com>
2022-07-21 12:50:00 +0900

    used generic

--- csBasic/src/csAdvanced/dataStructureIntroduction/binaryTree/preOrderWalk/Main.java
+++ csBasic/src/csAdvanced/dataStructureIntroduction/binaryTree/preOrderWalk/Main.java
@@ -2,17 +2,16 @@ package csAdvanced.dataStructureIntroduction.binaryTree.preOrderWalk;
 
 import java.util.Arrays;
 
-class BinaryTree{
+class BinaryTree<E>{
     public int data;
-    public BinaryTree left;
-    public BinaryTree right;
+    public BinaryTree<E> left;
+    public BinaryTree<E> right;
 
     public BinaryTree(int data){ this.data = data;}
-    public BinaryTree(int data, BinaryTree left, BinaryTree right){
+    public BinaryTree(int data, BinaryTree<E> left, BinaryTree<E> right){
         this.data = data;
         this.left = left;
         this.right = right;
-
     }
 
     public void printInOrder(){
@@ -26,7 +25,7 @@ class BinaryTree{
     }
 
     // 間順 (in-order) (LNR)
-    public void inOrderWalk(BinaryTree tRoot){
+    public void inOrderWalk(BinaryTree<E> tRoot){
         if(tRoot!=null){
             this.inOrderWalk(tRoot.left);
             System.out.print(tRoot.data + " ");
@@ -35,7 +34,7 @@ class BinaryTree{
     }
 
     // 前順（pre-order）（NLR）
-    public void preOrderWalk(BinaryTree tRoot){
+    public void preOrderWalk(BinaryTree<E> tRoot){
         if(tRoot!=null){
             System.out.print(tRoot.data + " ");
             this.preOrderWalk(tRoot.left);
@@ -44,8 +43,8 @@ class BinaryTree{
     }
 }
 
-class BinarySearchTree{
-    public BinaryTree root;
+class BinarySearchTree<E>{
+    private BinaryTree<E> root;
 
     public BinarySearchTree(int[] arr){
         Arrays.sort(arr);
@@ -53,24 +52,24 @@ class BinarySearchTree{
     }
 
 
-    private static BinaryTree sortedArrayToBSTHelper(int[] arr, int start, int end){
-        if(start == end) return new BinaryTree(arr[start], null, null);
+    private BinaryTree<E> sortedArrayToBSTHelper(int[] arr, int start, int end){
+        if(start == end) return new BinaryTree<E>(arr[start], null, null);
 
         int mid = (int)Math.floor((start+end)/2);
 
-        BinaryTree left = null;
+        BinaryTree<E> left = null;
         if(start <= mid-1) left = sortedArrayToBSTHelper(arr, start, mid-1);
 
-        BinaryTree right = null;
+        BinaryTree<E> right = null;
         if(end >= mid+1) right = sortedArrayToBSTHelper(arr, mid+1, end);
 
-        BinaryTree root = new BinaryTree(arr[mid], left, right);
+        BinaryTree<E> root = new BinaryTree<E>(arr[mid], left, right);
 
         return root;
     }
 
     public boolean keyExist(int key){
-        BinaryTree iterator = this.root;
+        BinaryTree<E> iterator = this.root;
         while(iterator != null){
             if(iterator.data == key) return true;
             if(iterator.data < key) iterator = iterator.right;
@@ -79,8 +78,8 @@ class BinarySearchTree{
         return false;
     }
 
-    public BinaryTree search(int key){
-        BinaryTree iterator = this.root;
+    public BinaryTree<E> search(int key){
+        BinaryTree<E> iterator = this.root;
         while(iterator != null){
             if(iterator.data == key) return iterator;
             if(iterator.data < key) iterator = iterator.right;
@@ -96,8 +95,8 @@ class BinarySearchTree{
 
 class Main{
     public static void main(String[] args){
-        BinarySearchTree balancedBST = new BinarySearchTree(new int[]{1,2,3,4,5,6,7,8,9,10,11});
-        BinarySearchTree balancedBST2 = new BinarySearchTree(new int[]{4,43,36,46,32,7,97,95,34,8,96,35,85,1010,232});
+        BinarySearchTree<Integer> balancedBST = new BinarySearchTree<>(new int[]{1,2,3,4,5,6,7,8,9,10,11});
+        BinarySearchTree<Integer> balancedBST2 = new BinarySearchTree<>(new int[]{4,43,36,46,32,7,97,95,34,8,96,35,85,1010,232});
         balancedBST.printSorted();
         balancedBST2.printSorted();
     }
